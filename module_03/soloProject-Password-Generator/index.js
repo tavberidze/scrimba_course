737d799101c1855d4cb220b8a911ae013ed6dd5c
tavberidze <101591883+tavberidze@users.noreply.github.com>
2022-07-19 12:42:59 +0400

    intial upload

---
+++ module_03/soloProject-Password-Generator/index.js
@@ -0,0 +1,15 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordOne = document.getElementsByClassName("output-el");
+let passwordTwo = document.getElementsByClassName("two");
+
+
+
+function generatePassword() {
+    for (let i = 0; i < 16; i++){
+        i = Math.floor((Math.random() * characters.length) + 1);  
+        passwordOne.textcontent = i;
+    }
+}
